c2a604f1be365f3ec4d5fde73ee8d6684ce57889
drusty93-wq <drusty93@gmail.com>
2026-01-20 16:14:08 +0000

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "36853366"
+    serviceWorkerVersion: "4280719955"
   }
 });
